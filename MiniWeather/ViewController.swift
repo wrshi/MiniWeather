a0cdd7172d320e6f9b958444915e919e42afdf0a
Weiran Shi <wrshi313@gmail.com>
2015-12-03 20:43:53 -0800

    Initial Commit

---
+++ MiniWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniWeather
+//
+//  Created by Weiran Shi on 2015-12-03.
+//  Copyright (c) 2015 Vera Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
